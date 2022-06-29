025a0404ac5c61149d84a6ed412ec4eb5a6bc6c5
Arko Elsenaar <a.elsenaar@situatiewin.nl>
2022-06-29 23:20:12 +0200

    FeelsJorqensenMan

--- index.php
+++ index.php
@@ -12,21 +12,41 @@
             <div script="install TabContainer">
 
                 <div role="tablist">
-                    <button role="tab" id="tab-1" aria-controls="panel-1" aria-selected="true">View quotes</button>
+                    <button role="tab" id="tab-1" aria-controls="panel-1" aria-selected="true">View quote</button>
                 </div>
         
                 <div role="tabpanel" id="panel-1" aria-labelledby="tab-1">
                     <div id="quotes">
 
                     </div>
-                    <button _="on click show the next <progress/> then wait 5s then fetch ./quotes1.mark then put the result into the previous <div/> then wait 1s then hide the <progress/>">Load quotes</button>
+                    <button _="on click show the next <progress/> then wait 5s then fetch ./backend.php?endpoint=postquote then put the result into the previous <div/> then wait 1s then hide the <progress/>">Load quotes</button>
                     <progress style="display: none;float:right;"></progress>
                 </div>
                 <div role="tabpanel" id="panel-2" aria-labelledby="tab-2" hidden=""><p>This is the content for the second tab</p></div>
                 <div role="tabpanel" id="panel-3" aria-labelledby="tab-3" hidden=""><p>This is the content for the third tab</p></div>
             </div>
-        </main>
+            <div script="install TabContainer">
+                <div role="tablist">
+                    <button role="tab" id="tab-1" aria-controls="panel-1" aria-selected="true">Submit quote</button>
+                </div>
+        
+                <div role="tabpanel" id="panel-1" aria-labelledby="tab-1">
+                <form class="box rows" hx-post="./backend.php?endpoint=getquote">
+    <p>
+    <label for="blockinput">My input</label>
+    <input type="text" id="blockinput" name="field">
 
-        <template url="./quotes1.mark" delay="1000"></template>
+    </p><p>
+    <label for="blockinput2">My other input</label>
+    <textarea name="field2"></textarea>
+</p>
+
+<button type="submit" >submit</button>
+</form>
+                    <progress style="display: none;float:right;"></progress>
+                </div>
+                
+            </div>
+        </main>
     </body>
 </html>
